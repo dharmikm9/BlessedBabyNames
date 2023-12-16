ca51c041f897cfa71467895cdca29babb891afa4
dharmikm9 <mehtadharmik26101998@gmail.com>
2023-12-16 14:00:19 +0530

    Update contact_us.py - replace linkedin badge

--- contact_us.py
+++ contact_us.py
@@ -9,8 +9,13 @@ def contactus_page():
     col1, col2 = st.columns([1, 3])
 
     with col1:
-        st.image("https://kit8.net/wp-content/uploads/edd/2022/04/robots_and_humans_communication_preview.jpg",
-                 width=200)  # Replace with your image URL
+        # st.image("https://kit8.net/wp-content/uploads/edd/2022/04/robots_and_humans_communication_preview.jpg",
+                 # width=200)  # Replace with your image URL
+        components.html("""
+         <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+         <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="dharmikm9" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/dharmikm9?trk=profile-badge">Dharmik Mehta</a></div>
+        """, )
+        
     with col2:
         st.header("Meet the Developer")
         st.write("""
@@ -18,10 +23,7 @@ def contactus_page():
             Feel free to connect with me through the following social media platforms:
             """)
         
-        components.html("""
-         <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-         <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="dharmikm9" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/dharmikm9?trk=profile-badge">Dharmik Mehta</a></div>
-        """, )
+      
         
         soc_col1, soc_col2 = st.columns([1, 3])
 
