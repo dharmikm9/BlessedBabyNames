ddf1b902d5bc5ecf3b4ca17847fbd1eea6ac9c43
dharmikm9 <mehtadharmik26101998@gmail.com>
2023-12-16 14:11:45 +0530

    Update contact_us.py

--- contact_us.py
+++ contact_us.py
@@ -4,26 +4,14 @@ import streamlit.components.v1 as components
 def contactus_page():
     st.title("Developer Contact Info")
 
-
-
     col1, col2 = st.columns([0.5, 0.5])
 
     with col1:
-        # st.image("https://kit8.net/wp-content/uploads/edd/2022/04/robots_and_humans_communication_preview.jpg",
-                 # width=200)  # Replace with your image URL
-        components.html("""
-         <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-         <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="dharmikm9" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/dharmikm9?trk=profile-badge"></a></div>
-        """, width=300, height=400)
-        
-    with col2:
         st.header("Meet the Developer")
         st.write("""
             Hello! I'm Dharmik, a passionate developer interested in Machine Learning & ML.
             Feel free to connect with me through the following social media platforms:
             """)
-        
-      
         
         soc_col1, soc_col2 = st.columns([1, 3])
 
@@ -35,3 +23,10 @@ def contactus_page():
             st.markdown(
                 "[![GitHub](https://img.shields.io/badge/GitHub-Profile-lightgrey)](https://github.com/dharmikm9)"
             )
+   with col2:
+            # st.image("https://kit8.net/wp-content/uploads/edd/2022/04/robots_and_humans_communication_preview.jpg",
+                     # width=200)  # Replace with your image URL
+            components.html("""
+             <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+             <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="dharmikm9" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/dharmikm9?trk=profile-badge"></a></div>
+            """, width=300, height=400)
